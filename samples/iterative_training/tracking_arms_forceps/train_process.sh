09f1b34daeb07f36a19b238f0c42386fa3e5a739
trevol <iamvovan@mail.ru>
2020-01-19 12:18:29 +0300

    train and re-detect

--- samples/iterative_training/tracking_arms_forceps/train_process.sh
+++ samples/iterative_training/tracking_arms_forceps/train_process.sh
@@ -1,2 +1,6 @@
-python ./train_for_arm_tracking.py --lr=.001 --max-sessions=10
+python ./train_for_arm_tracking.py --lr=.001 --max-sessions=3
+python ./process_video_frames.py --images_per_gpu=8
+python ./train_for_arm_tracking.py --lr=.001 --max-sessions=3
+python ./process_video_frames.py --images_per_gpu=8
+python ./train_for_arm_tracking.py --lr=.001 --max-sessions=3
 python ./process_video_frames.py --images_per_gpu=8
